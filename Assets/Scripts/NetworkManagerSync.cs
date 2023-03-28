93bcbbc7b58155b9d2cd9c03a3f3f354d48ad54b
amiroo54 <amiroo.f54@gmail.com>
2023-03-29 02:07:49 +0330

    added syncperrel packages to the project.

---
+++ Assets/Scripts/NetworkManagerSync.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+using ParrelSync;
+public class NetworkManagerSync : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (ClonesManager.IsClone())
+        {
+            NetworkManager.Singleton.StartClient();
+        }
+        else
+        {
+            NetworkManager.Singleton.StartHost();
+        }
+    }
+
+   
+}
